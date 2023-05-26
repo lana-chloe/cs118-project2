cb620bebd59e2480055a7e1bdd7747676f5a0775
Lana Lim <lanachloe@g.ucla.edu>
2023-05-26 04:55:21 +0000

    TCP setup. Terminating each client might not be implemented correctly.

--- project/main.cpp
+++ project/main.cpp
@@ -64,104 +64,95 @@ void configureNAPT() {
   int line = 1;
 
   while (getline(cin, szLine)) {
-      // first line is the router's LAN IP and the WAN IP
-      if (line == 1) {
-        size_t dwPos = szLine.find(' ');
-        rLANaddr = szLine.substr(0, dwPos);
-        rWANaddr = szLine.substr(dwPos + 1);
-<<<<<<< HEAD
-        //cout << "rLANaddr: " << rLANaddr << endl;
-        //cout << "rWANaddr: " << rWANaddr << endl;
-=======
-        cout << "rLANaddr: " << rLANaddr << endl;
-        cout << "rWANaddr: " << rWANaddr << endl;
+    // first line is the router's LAN IP and the WAN IP
+    if (line == 1) {
+      size_t dwPos = szLine.find(' ');
+      rLANaddr = szLine.substr(0, dwPos);
+      rWANaddr = szLine.substr(dwPos + 1);
+      //cout << "rLANaddr: " << rLANaddr << endl;
+      //cout << "rWANaddr: " << rWANaddr << endl;
 
-        //After getting router's LAN IP, set rLANsubnet to the first 3 bytes of rLANaddr:
-        size_t firstdot = rLANaddr.find('.'); // first . char
-        size_t seconddot = rLANaddr.find('.', firstdot + 1); // second . char
-        size_t thirddot = rLANaddr.find('.', seconddot + 1); // third . char
-        rLANsubnet = rLANaddr.substr(0, thirddot);
+      //After getting router's LAN IP, set rLANsubnet to the first 3 bytes of rLANaddr:
+      size_t firstdot = rLANaddr.find('.'); // first . char
+      size_t seconddot = rLANaddr.find('.', firstdot + 1); // second . char
+      size_t thirddot = rLANaddr.find('.', seconddot + 1); // third . char
+      rLANsubnet = rLANaddr.substr(0, thirddot);
 
-        cout << "rLANsubnet: " << rLANsubnet << endl;
->>>>>>> 14666ec68fc7527668102b7a5f7edbf90d86a142
-      }
-      // get to NAPT table section
-      else if (szLine.find(' ') != string::npos) {
-        size_t first = szLine.find(' '); // first space char
-        size_t second = szLine.find(' ', first + 1); // second space char
-        size_t end = szLine.size();
+      cout << "rLANsubnet: " << rLANsubnet << endl;
+    }
+    // get to NAPT table section
+    else if (szLine.find(' ') != string::npos) {
+      size_t first = szLine.find(' '); // first space char
+      size_t second = szLine.find(' ', first + 1); // second space char
+      size_t end = szLine.size();
 
-        // extract three substrings
-        string LANaddr = szLine.substr(0, first);
-        string LANprt = szLine.substr(first + 1, second - first - 1);
-        string WANprt = szLine.substr(second + 1, end - second - 2);
+      // extract three substrings
+      string LANaddr = szLine.substr(0, first);
+      string LANprt = szLine.substr(first + 1, second - first - 1);
+      string WANprt = szLine.substr(second + 1, end - second - 2);
 
-        //cout << "LANaddr: " << LANaddr << endl;
-        //cout << "LANprt: " << LANprt << endl;
-        //cout << "WANprt: " << WANprt << endl;
+      //cout << "LANaddr: " << LANaddr << endl;
+      //cout << "LANprt: " << LANprt << endl;
+      //cout << "WANprt: " << WANprt << endl;
 
-        // add to NAPT table
-        NAPTEntry entry(LANaddr, LANprt, WANprt);
-        table.push_back(entry);
-      }
+      // add to NAPT table
+      NAPTEntry entry(LANaddr, LANprt, WANprt);
+      table.push_back(entry);
+    }
 
-      line++;
+    line++;
   }
 }
-int findEntry(string WANprt) //returns the index of the entry in the table that matches WANprt, -1 otherwise
-{
-    for (unsigned int i = 0; i < table.size(); i++)
-    {
-        if (WANprt.compare(table.at(i).WANprt) == 0)
-        {
-            return i;
-        }
+
+// returns the index of the entry in the table that matches WANprt, -1 otherwise 
+int findEntry(string WANprt) {
+  for (unsigned int i = 0; i < table.size(); i++) {
+    if (WANprt.compare(table.at(i).WANprt) == 0) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
-int findEntry(string LANaddr, string LANprt) //returns the index of the entry in the table that matches (LANaddr, LANprt), -1 otherwise
-{
-    for (unsigned int i = 0; i < table.size(); i++)
-    {
-        if (LANprt.compare(table.at(i).LANprt) == 0 && LANaddr.compare(table.at(i).LANaddr) == 0)
-        {
-            return i;
-        }
+// returns the index of the entry in the table that matches (LANaddr, LANprt), -1 otherwise
+int findEntry(string LANaddr, string LANprt) {
+  for (unsigned int i = 0; i < table.size(); i++) {
+    if (LANprt.compare(table.at(i).LANprt) == 0 && LANaddr.compare(table.at(i).LANaddr) == 0) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
-int getType(string sAddr, string dAddr) //Compare src and dest addr to rLANsubnet, return 0 if LAN to LAN, 1 if LAN to WAN, and 2 if WAN to LAN
-{ //NOTE: no checking of whether addresses are valid; classify an address as LAN if the 1st three bytes match rLANsubnet, classify as WAN otherwise
-    size_t firstdot = sAddr.find('.'); // first . char
-    size_t seconddot = sAddr.find('.', firstdot + 1); // second . char
-    size_t thirddot = sAddr.find('.', seconddot + 1); // third . char
+int getType(string sAddr, string dAddr) { // compare src and dest addr to rLANsubnet, return 0 if LAN to LAN, 1 if LAN to WAN, and 2 if WAN to LAN
+//NOTE: no checking of whether addresses are valid; classify an address as LAN if the 1st three bytes match rLANsubnet, classify as WAN otherwise
+  size_t firstdot = sAddr.find('.'); // first . char
+  size_t seconddot = sAddr.find('.', firstdot + 1); // second . char
+  size_t thirddot = sAddr.find('.', seconddot + 1); // third . char
 
-    string truncSaddr = sAddr.substr(0, thirddot);
+  string truncSaddr = sAddr.substr(0, thirddot);
 
-    firstdot = dAddr.find('.'); // first . char
-    seconddot = dAddr.find('.', firstdot + 1); // second . char
-    thirddot = dAddr.find('.', seconddot + 1); // third . char
+  firstdot = dAddr.find('.'); // first . char
+  seconddot = dAddr.find('.', firstdot + 1); // second . char
+  thirddot = dAddr.find('.', seconddot + 1); // third . char
 
-    string truncDaddr = dAddr.substr(0, thirddot);
+  string truncDaddr = dAddr.substr(0, thirddot);
 
-    int src = 0;
-    int dst = 0;
-    if (rLANsubnet.compare(truncSaddr) == 0)
-        src = 1;
-    if (rLANsubnet.compare(truncDaddr) == 0)
-        dst = 1;
-    
-    if (src == 1 && dst == 1) //LAN to LAN
-        return 0;
-    else if (src == 1 && dst == 0) //LAN to WAN
-        return 1;
-    else //WAN to LAN
-        return 2;
+  int src = 0;
+  int dst = 0;
+  if (rLANsubnet.compare(truncSaddr) == 0)
+    src = 1;
+  if (rLANsubnet.compare(truncDaddr) == 0)
+    dst = 1;
+  
+  if (src == 1 && dst == 1) //LAN to LAN
+    return 0;
+  else if (src == 1 && dst == 0) //LAN to WAN
+    return 1;
+  else //WAN to LAN
+    return 2;
 }
 
-<<<<<<< HEAD
 int handleClient(int clientSocket) {
   char buf[BUF_SIZE];
   int bytesRead;
@@ -186,6 +177,9 @@ int handleClient(int clientSocket) {
 }
 
 int main() {
+  // configure NAPT table
+  configureNAPT();
+
   // TCP setup
   int serverSocket, maxSocket, activity, clientSocket;
   struct sockaddr_in serverAddress, clientAddress;
@@ -279,9 +273,6 @@ int main() {
   }
 
   close(serverSocket);
-=======
-int main() {
-  configureNAPT();
 
   /*cout << "LAN to LAN returns type " << getType("192.168.1.200", "192.168.1.100") << endl;
   cout << "LAN to WAN returns type " << getType("192.168.1.200", rWANaddr) << endl;
@@ -299,7 +290,6 @@ int main() {
   cout << "Entry for LANaddr 192.168.1.300 and LANprt 8080 is found at " << findEntry("192.168.1.300", "8080") << endl;
   cout << "Entry for LANaddr 192.168.1.100 and LANprt 8080 is found at " << findEntry("192.168.1.100", "8080") << endl;
   cout << "Entry for LANaddr 192.168.1.200 and LANprt 9000 is found at " << findEntry("192.168.1.200", "9000") << endl;*/
->>>>>>> 14666ec68fc7527668102b7a5f7edbf90d86a142
 
   return 0;
 }
