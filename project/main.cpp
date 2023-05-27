8be7cd488893d6cb2a992966d8b951cd6a0abdea
Lana Lim <lanachloe@g.ucla.edu>
2023-05-27 21:30:54 +0000

    Parse IP header and protocol header. Did not fully implement checksum yet.

--- project/main.cpp
+++ project/main.cpp
@@ -10,6 +10,9 @@
 #include <cstdlib>
 #include <arpa/inet.h>
 #include <sys/select.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
 using namespace std;
 
 #define PORT 5152
@@ -17,22 +20,13 @@ using namespace std;
 #define BUF_SIZE 1024
 
 // IPv4 packet struct
-struct IPv4Packet {
-  string srcAddr;
-  string destAddr;
-  uint8_t TTL;
-  uint16_t checksum;
-
-  int type;
+// not sure how to implement this yet
+// should have a int type field such that:
   // 0 = LAN to LAN
   // 1 = LAN to WAN
   // 2 = WAN to LAN
 
-  IPv4Packet(string sAddr, string dAddr, uint8_t TTL, uint16_t cs, int tp)
-    : srcAddr(sAddr), destAddr(dAddr), TTL(TTL), checksum(cs), type(tp) {}
-};
-
-// NAPT table entry struct
+// NAPT table entry 
 struct NAPTEntry {
   string LANaddr;
   string LANprt;
@@ -42,7 +36,7 @@ struct NAPTEntry {
     : LANaddr(Laddr), LANprt(Lprt), WANprt(Wprt) {}
 };
 
-// LAN IPs table entry struct
+// LAN IPs table entry 
 struct LANEntry {
   string LANaddr;
   string LANprt;
@@ -51,134 +45,58 @@ struct LANEntry {
     : LANaddr(Laddr), LANprt(Lprt) {}
 };
 
+// UDP packet
+struct UDPPacket {
+  uint16_t sourcePort;
+  uint16_t destinationPort;
+  uint16_t checksum;
+};
+
+// TCP packet
+struct TCPPacket {
+  uint16_t sourcePort;
+  uint16_t destinationPort;
+  uint16_t checksum;
+};
+
 // NAPT Table
 vector<NAPTEntry> table;
 
-// router LAN and WAN addresses
+// original router LAN and WAN addresses
 string rLANaddr;
 string rWANaddr;
 string rLANsubnet;
 
-void configureNAPT() {
-  string szLine;
-  int line = 1;
-
-  while (getline(cin, szLine)) {
-    // first line is the router's LAN IP and the WAN IP
-    if (line == 1) {
-      size_t dwPos = szLine.find(' ');
-      rLANaddr = szLine.substr(0, dwPos);
-      rWANaddr = szLine.substr(dwPos + 1);
-      //cout << "rLANaddr: " << rLANaddr << endl;
-      //cout << "rWANaddr: " << rWANaddr << endl;
-
-      //After getting router's LAN IP, set rLANsubnet to the first 3 bytes of rLANaddr:
-      size_t firstdot = rLANaddr.find('.'); // first . char
-      size_t seconddot = rLANaddr.find('.', firstdot + 1); // second . char
-      size_t thirddot = rLANaddr.find('.', seconddot + 1); // third . char
-      rLANsubnet = rLANaddr.substr(0, thirddot);
-
-      cout << "rLANsubnet: " << rLANsubnet << endl;
-    }
-    // get to NAPT table section
-    else if (szLine.find(' ') != string::npos) {
-      size_t first = szLine.find(' '); // first space char
-      size_t second = szLine.find(' ', first + 1); // second space char
-      size_t end = szLine.size();
-
-      // extract three substrings
-      string LANaddr = szLine.substr(0, first);
-      string LANprt = szLine.substr(first + 1, second - first - 1);
-      string WANprt = szLine.substr(second + 1, end - second - 2);
-
-      //cout << "LANaddr: " << LANaddr << endl;
-      //cout << "LANprt: " << LANprt << endl;
-      //cout << "WANprt: " << WANprt << endl;
-
-      // add to NAPT table
-      NAPTEntry entry(LANaddr, LANprt, WANprt);
-      table.push_back(entry);
-    }
-
-    line++;
-  }
-}
-
-// returns the index of the entry in the table that matches WANprt, -1 otherwise 
-int findEntry(string WANprt) {
-  for (unsigned int i = 0; i < table.size(); i++) {
-    if (WANprt.compare(table.at(i).WANprt) == 0) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-// returns the index of the entry in the table that matches (LANaddr, LANprt), -1 otherwise
-int findEntry(string LANaddr, string LANprt) {
-  for (unsigned int i = 0; i < table.size(); i++) {
-    if (LANprt.compare(table.at(i).LANprt) == 0 && LANaddr.compare(table.at(i).LANaddr) == 0) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-int getType(string sAddr, string dAddr) { // compare src and dest addr to rLANsubnet, return 0 if LAN to LAN, 1 if LAN to WAN, and 2 if WAN to LAN
-//NOTE: no checking of whether addresses are valid; classify an address as LAN if the 1st three bytes match rLANsubnet, classify as WAN otherwise
-  size_t firstdot = sAddr.find('.'); // first . char
-  size_t seconddot = sAddr.find('.', firstdot + 1); // second . char
-  size_t thirddot = sAddr.find('.', seconddot + 1); // third . char
-
-  string truncSaddr = sAddr.substr(0, thirddot);
-
-  firstdot = dAddr.find('.'); // first . char
-  seconddot = dAddr.find('.', firstdot + 1); // second . char
-  thirddot = dAddr.find('.', seconddot + 1); // third . char
-
-  string truncDaddr = dAddr.substr(0, thirddot);
-
-  int src = 0;
-  int dst = 0;
-  if (rLANsubnet.compare(truncSaddr) == 0)
-    src = 1;
-  if (rLANsubnet.compare(truncDaddr) == 0)
-    dst = 1;
-  
-  if (src == 1 && dst == 1) //LAN to LAN
-    return 0;
-  else if (src == 1 && dst == 0) //LAN to WAN
-    return 1;
-  else //WAN to LAN
-    return 2;
-}
-
-int handleClient(int clientSocket) {
-  char buf[BUF_SIZE];
-  int bytesRead;
-
-  bytesRead = read(clientSocket, buf, sizeof(buf));
-
-  if (bytesRead < 0) {
-    perror("Error reading from socket");
-    close(clientSocket);
-    return 1;
-  }
-
-  if (bytesRead == 0) {
-    // client disconnected
-    close(clientSocket);
-    return 1;
-  }
-
-  buf[bytesRead] = '\0';
-  cout << "Received message from client " << clientSocket << ": " << buf << endl;
-  return 0;
-}
+// FUNCTION DECLARATIONS //
+void configureNAPT();
+int findEntry(string WANprt); // returns the index of the entry in the table that matches WANprt, -1 otherwise 
+int findEntry(string LANaddr, string LANprt); // returns the index of the entry in the table that matches (LANaddr, LANprt), -1 otherwise
+int getType(string sAddr, string dAddr); // compare src and dest addr to rLANsubnet, return 0 if LAN to LAN, 1 if LAN to WAN, and 2 if WAN to LAN
+UDPPacket parseUDPPacket(const char* buffer); // parse UDP packet
+TCPPacket parseTCPPacket(const char* buffer); // parse TCP packet
+void parseIPPacket(const char* buffer); // parse IP packet
+uint16_t calculateChecksum(const void* data, size_t length); // calculate checksum
+bool verifyChecksum(uint16_t checksum, const void* data, size_t length); // compare calculated checksum with checksum field
+int handleClient(int clientSocket); // get message from client socket
 
 int main() {
   // configure NAPT table
-  configureNAPT();
+  //configureNAPT();
+
+  /*cout << "LAN to LAN returns type " << getType("192.168.1.200", "192.168.1.100") << endl;
+  cout << "LAN to WAN returns type " << getType("192.168.1.200", rWANaddr) << endl;
+  cout << "LAN to LAN returns type " << getType("192.168.1.200", rLANaddr) << endl;
+  cout << "WAN to LAN returns type " << getType(rWANaddr, rLANaddr) << endl;*/
+
+  /*cout << "Entry for WANprt # 8080 is found at " << findEntry("8080") << endl;
+  cout << "Entry for WANprt # 443 is found at " << findEntry("443") << endl;
+  cout << "Entry for WANprt # 123 is found at " << findEntry("123") << endl;
+
+  cout << "\n" << endl;
+
+  cout << "Entry for LANaddr 192.168.1.300 and LANprt 8080 is found at " << findEntry("192.168.1.300", "8080") << endl;
+  cout << "Entry for LANaddr 192.168.1.100 and LANprt 8080 is found at " << findEntry("192.168.1.100", "8080") << endl;
+  cout << "Entry for LANaddr 192.168.1.200 and LANprt 9000 is found at " << findEntry("192.168.1.200", "9000") << endl;*/
 
   // TCP setup
   int serverSocket, maxSocket, activity, clientSocket;
@@ -274,22 +192,175 @@ int main() {
 
   close(serverSocket);
 
-  /*cout << "LAN to LAN returns type " << getType("192.168.1.200", "192.168.1.100") << endl;
-  cout << "LAN to WAN returns type " << getType("192.168.1.200", rWANaddr) << endl;
-  cout << "LAN to LAN returns type " << getType("192.168.1.200", rLANaddr) << endl;
-  cout << "WAN to LAN returns type " << getType(rWANaddr, rLANaddr) << endl;*/
+  return 0;
+}
 
+void configureNAPT() {
+  string szLine;
+  int line = 1;
 
+  while (getline(cin, szLine)) {
+    // first line is the router's LAN IP and the WAN IP
+    if (line == 1) {
+      size_t dwPos = szLine.find(' ');
+      rLANaddr = szLine.substr(0, dwPos);
+      rWANaddr = szLine.substr(dwPos + 1);
+      //cout << "rLANaddr: " << rLANaddr << endl;
+      //cout << "rWANaddr: " << rWANaddr << endl;
 
-  /*cout << "Entry for WANprt # 8080 is found at " << findEntry("8080") << endl;
-  cout << "Entry for WANprt # 443 is found at " << findEntry("443") << endl;
-  cout << "Entry for WANprt # 123 is found at " << findEntry("123") << endl;
+      //After getting router's LAN IP, set rLANsubnet to the first 3 bytes of rLANaddr:
+      size_t firstdot = rLANaddr.find('.'); // first . char
+      size_t seconddot = rLANaddr.find('.', firstdot + 1); // second . char
+      size_t thirddot = rLANaddr.find('.', seconddot + 1); // third . char
+      rLANsubnet = rLANaddr.substr(0, thirddot);
 
-  cout << "\n" << endl;
+      cout << "rLANsubnet: " << rLANsubnet << endl;
+    }
+    // get to NAPT table section
+    else if (szLine.find(' ') != string::npos) {
+      size_t first = szLine.find(' '); // first space char
+      size_t second = szLine.find(' ', first + 1); // second space char
+      size_t end = szLine.size();
 
-  cout << "Entry for LANaddr 192.168.1.300 and LANprt 8080 is found at " << findEntry("192.168.1.300", "8080") << endl;
-  cout << "Entry for LANaddr 192.168.1.100 and LANprt 8080 is found at " << findEntry("192.168.1.100", "8080") << endl;
-  cout << "Entry for LANaddr 192.168.1.200 and LANprt 9000 is found at " << findEntry("192.168.1.200", "9000") << endl;*/
+      // extract three substrings
+      string LANaddr = szLine.substr(0, first);
+      string LANprt = szLine.substr(first + 1, second - first - 1);
+      string WANprt = szLine.substr(second + 1, end - second - 2);
+
+      //cout << "LANaddr: " << LANaddr << endl;
+      //cout << "LANprt: " << LANprt << endl;
+      //cout << "WANprt: " << WANprt << endl;
+
+      // add to NAPT table
+      NAPTEntry entry(LANaddr, LANprt, WANprt);
+      table.push_back(entry);
+    }
+
+    line++;
+  }
+}
+
+int findEntry(string WANprt) {
+  for (unsigned int i = 0; i < table.size(); i++) {
+    if (WANprt.compare(table.at(i).WANprt) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int findEntry(string LANaddr, string LANprt) {
+  for (unsigned int i = 0; i < table.size(); i++) {
+    if (LANprt.compare(table.at(i).LANprt) == 0 && LANaddr.compare(table.at(i).LANaddr) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// NOTE: no checking of whether addresses are valid; classify an address as LAN if the 1st three bytes match rLANsubnet, classify as WAN otherwise
+int getType(string sAddr, string dAddr) { 
+  size_t firstdot = sAddr.find('.'); // first . char
+  size_t seconddot = sAddr.find('.', firstdot + 1); // second . char
+  size_t thirddot = sAddr.find('.', seconddot + 1); // third . char
+
+  string truncSaddr = sAddr.substr(0, thirddot);
+
+  firstdot = dAddr.find('.'); // first . char
+  seconddot = dAddr.find('.', firstdot + 1); // second . char
+  thirddot = dAddr.find('.', seconddot + 1); // third . char
+
+  string truncDaddr = dAddr.substr(0, thirddot);
+
+  int src = 0;
+  int dst = 0;
+  if (rLANsubnet.compare(truncSaddr) == 0)
+    src = 1;
+  if (rLANsubnet.compare(truncDaddr) == 0)
+    dst = 1;
+  
+  if (src == 1 && dst == 1) //LAN to LAN
+    return 0;
+  else if (src == 1 && dst == 0) //LAN to WAN
+    return 1;
+  else //WAN to LAN
+    return 2;
+}
+
+UDPPacket parseUDPPacket(const char* buffer) {
+  const udphdr* udpHeader = reinterpret_cast<const udphdr*>(buffer);
+
+  UDPPacket udpPacket;
+  udpPacket.sourcePort = ntohs(udpHeader->source);
+  udpPacket.destinationPort = ntohs(udpHeader->dest);
+  udpPacket.checksum = ntohs(udpHeader->check);
+
+  return udpPacket;
+}
+
+TCPPacket parseTCPPacket(const char* buffer) {
+  const tcphdr* tcpHeader = reinterpret_cast<const tcphdr*>(buffer);
+
+  TCPPacket tcpPacket;
+  tcpPacket.sourcePort = ntohs(tcpHeader->source);
+  tcpPacket.destinationPort = ntohs(tcpHeader->dest);
+  tcpPacket.checksum = ntohs(tcpHeader->check);
+
+  return tcpPacket;
+}
+
+void parseIPPacket(const char* buffer) {
+  const ip* ipHeader = reinterpret_cast<const ip*>(buffer);
+
+  string sourceIP = inet_ntoa(ipHeader->ip_src);
+  string destinationIP = inet_ntoa(ipHeader->ip_dst);
+  uint16_t checksum = ntohs(ipHeader->ip_sum);
+  uint8_t protocol = ipHeader->ip_p;
+
+  if (protocol == IPPROTO_UDP) {
+    UDPPacket udpPacket = parseUDPPacket(buffer + ipHeader->ip_hl * 4);
+    // check what IPv4 type it is
+  } else if (protocol == IPPROTO_TCP) {
+    TCPPacket tcpPacket = parseTCPPacket(buffer + ipHeader->ip_hl * 4);
+    // check what IPv4 type it is
+  } else {
+    perror("Unknown Protocol");
+    exit(1);
+  }
+}
+
+uint16_t calculateChecksum(const void* data, size_t length) {
+  return 0;
+}
+
+bool verifyChecksum(uint16_t checksum, const void* data, size_t length) {
+  uint16_t calculatedChecksum = calculateChecksum(data, length);
+  return checksum == calculatedChecksum;
+}
+
+int handleClient(int clientSocket) {
+  char buffer[BUF_SIZE];
+  int bytesRead;
+
+  bytesRead = read(clientSocket, buffer, sizeof(buffer));
+
+  if (bytesRead < 0) {
+    perror("Error reading from socket");
+    close(clientSocket);
+    return 1;
+  }
+
+  if (bytesRead == 0) {
+    // client disconnected
+    close(clientSocket);
+    return 1;
+  }
+
+  buffer[bytesRead] = '\0';
+
+  cout << "Received message:\n" << buffer << endl;
+  
+  parseIPPacket(buffer);
 
   return 0;
 }
